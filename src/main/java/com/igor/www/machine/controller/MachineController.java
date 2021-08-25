b5d61e31510e5441195384ad3e548ee47c8cdf8c
Igor <igor95ns@gmail.com>
2021-08-25 15:01:22 +0200

    Fix http method for search (GET)

--- src/main/java/com/igor/www/machine/controller/MachineController.java
+++ src/main/java/com/igor/www/machine/controller/MachineController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -93,9 +94,9 @@ public class MachineController {
 		}
     }
 	
-	@PostMapping("/search")
+	@GetMapping("/search")
     public ResponseEntity<List<MachineDto>> search(@RequestParam(name = "status", defaultValue = "RUNNING") Status status) {
-        final List<MachineDto> filtered = machineService.searchByStatus(status);
+        final List<MachineDto> filtered = machineService.searchByStatus(status); // TODO - need to implement fetching by user
         return new ResponseEntity<>(filtered, HttpStatus.OK);
     }
 	
