429646f8aae2a448dbd41ac7d7c743f1c9f5fee6
Igor <igor95ns@gmail.com>
2021-08-25 11:29:15 +0200

    Initial commit

---
+++ src/main/java/com/igor/www/QuantoxTestApplication.java
@@ -0,0 +1,13 @@
+package com.igor.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantoxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantoxTestApplication.class, args);
+	}
+
+}
